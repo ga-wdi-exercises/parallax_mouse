654e5d7e222b7caee7277badb792766e2a8d5712
Lisa Anne Burr <burr_lisa@bah.com>
2017-02-03 11:09:19 -0500

    completed Commit 2

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,6 +1,12 @@
-/*Commit 1
-When the mouse moves within the body, console.log the x and y coordinates. The docs on mousemove may be helpful here.*/
+//Commit 1 -  When the mouse moves within the body, console.log the x and y coordinates. The docs on mousemove may be helpful here.
+
+// $("body").mouseover(function(event){
+//   console.log(event.pageX, event.pageY);
+// })
+
+//Commit 2 - Instead of using console.log, replace the text of the h1 with the current coordinates of the mouse.
 
 $("body").mouseover(function(event){
-  console.log(event.pageX, event.pageY);
+  var pointerCoords = event.pageX + ", " + event.pageY
+  $("h1").replaceWith("<h1>" + pointerCoords + "</h1>")
 })
