84e8f9763594537e87d76afb8eef1e9039cfb793
Hannah Schuster <hschuster9@gmail.com>
2017-02-03 11:29:06 -0500

    commit 3: changed image margins

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,6 +1,20 @@
 
 $("body").mousemove(function(event){
-  var coordinates = "( " + event.pageX + ", " + event.pageY + " )";
-  console.log(coordinates);
-  $("h1").text(coordinates);
-  })
+    var coordinates = "( " + event.pageX + ", " + event.pageY + " )";
+    var catXCoord = (event.pageX / 14);
+    var catYCoord =  (event.pageY / 20);
+
+    var dogXCoord = (event.pageX / 6);
+    var dogYCoord =  (event.pageY / 10);
+
+    var pandaXCoord = (event.pageX / 16);
+    var pandaYCoord =  (event.pageY / 12);
+
+
+    console.log(coordinates);
+
+    $("h1").text(coordinates);
+    $(".cat").css("margin-left", catXCoord).css("margin-top", catYCoord);
+    $(".dog").css("margin-left", dogXCoord).css("margin-top", dogYCoord);
+    $(".panda").css("margin-left", pandaXCoord).css("margin-top", pandaYCoord);
+    })
