87e71b5e570546071ec7eadb5d7efe504e1476a0
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-02-03 12:22:30 -0500

    commit2

--- catdogpanda.js
+++ catdogpanda.js
@@ -11,12 +11,19 @@
 
 //commit 1
 // When the mouse moves within the body, console.log the x and y coordinates. The docs on mousemove may be helpful here.
-$('body').mouseover(function(event){
-  console.log(event.pageX, event.pageY);
-})
+// $('body').mouseover(function(event){
+// })
+
 
 //commit 2
 //Instead of using console.log, replace the text of the h1 with the current coordinates of the mouse.
+
+$('body').mousemove(function(event){
+  $('#coords').text(event.pageX + ', ' + event.pageY);
+  console.log(event.pageX, event.pageY);
+
+})
+
 
 //Commit 3
 //When the mouse moves, change the css of each of your images so that the margin-left and margin-top change along with the x and y coordinates of your mouse. Read the docs on css. Try dividing the x and y coordinates by a factor (anywhere from 6 - 20) before setting your margin, and make this factor different for each image.
