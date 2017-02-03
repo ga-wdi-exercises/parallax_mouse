2f2516a14a7d6ec6a1c82dfa9d24967f1cd21e3b
Fiona <h.natapra@gmail.com>
2017-02-03 16:58:04 -0500

    commit 4 & bonus 1

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,9 +1,10 @@
+// COMMIT 1 & COMMIT 2
 $('body').mousemove(function(event){
   var coordinates = event.pageX + "," + event.pageY
    $('h1').text(coordinates);
   })
 
-
+// COMMIT 3
 $('body').mousemove(function(){
   $('.cat').css("margin-left",event.pageX/5).css("margin-top",event.pageY/5)
 })
@@ -16,6 +17,24 @@ $('body').mousemove(function(){
   $('.panda').css("margin-left",event.pageX/12).css("margin-top",event.pageY/12)
 })
 
+//COMMIT 4
+$('img').click(function(){
+  $(this).toggleClass('border')
+})
+
+// Bonus 1
+$('body').click(function(){
+  $('body').off()
+})
+
+// Bonus 2
 
 // "margin-left": event.PageX/10,
 //   "margin-top": event.PageY/10 }
+
+// var newClass = ('<class></class>')
+//
+//
+// $('.img').click(function(){
+//
+// })
