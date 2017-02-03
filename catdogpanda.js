940c5611a61535be248aa90fc185b0b76c1c9a2f
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-03 15:35:13 -0500

    updated title coordinates function

--- catdogpanda.js
+++ catdogpanda.js
@@ -8,8 +8,10 @@
 $('body').mousemove(function(event) {
   var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
  console.log(pageCoords)
-})
 
 // #2 Instead of using console.log, replace the text of the h1 with the current coordinates of the mouse.
 
-$("#coords").html("pageCoords");
+$("#coords").html(pageCoords);
+
+// #3 When the mouse moves, change the css of each of your images so that the margin-left and margin-top change along with the x and y coordinates of your mouse.
+})
