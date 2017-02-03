d609f05cbb29dce1ddc474101aa42d2e122f4cf6
Bedig Galladian <bgalladian@gmail.com>
2017-02-03 16:05:30 -0500

    got the images to move

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,4 +1,25 @@
 $ ('body').mousemove(function(event){
   var pageCoords = (event.pageX + "," + event.pageY);
-  $ ('h1').text(pageCoords);
-})
+  $ ('h1').text("Coordinates: " + pageCoords);
+  })
+
+$('body').mousemove(function(event) {
+  $(".cat").css("margin-left", event.pageX/ 6).css("margin-top", event.pageY/ 3);
+});
+
+$('body').mousemove(function(event) {
+  $(".dog").css("margin-left", event.pageX/ 20).css("margin-top", event.pageY/ 20);
+});
+
+$('body').mousemove(function(event) {
+  $(".panda").css("margin-left", event.pageX/ 15).css("margin-top", event.pageY/ 15);
+});
+
+
+
+
+// $(".cat").mouseover(function(){
+//   'margin-left': function(){
+//     return parsefloat(pageCoords);
+//   }
+// })
