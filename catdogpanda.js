32d289d400640213f7217216eabb1b3cd78b2f4d
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-03 10:37:52 -0500

    Commit 1: mouse moves, console logs x,y

---
+++ catdogpanda.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  // console.log($);
+  // console.log("hello world");
+
+  $("body").mousemove(function (e){
+      // var pos = $("body").position();
+      var page = event.pageX + ", " + event.pageY;
+      console.log(event.pageX + ", " + event.pageY); 
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
