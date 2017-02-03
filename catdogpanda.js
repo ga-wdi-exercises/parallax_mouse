736e9698f90c1777b42cb5aaa4ed517f93cca1e9
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-03 15:24:16 -0500

    making the page coordinates title change to the actual page coordinates

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,10 +1,15 @@
-// // When the mouse moves within the body, console.log the x and y coordinates.
+// My answer to #1
 // $( "body" ).mousemove(function( event ) {
 //   var mousemove = "Handler for .mousemove() called at ";
 //   mousemove += event.pageX + ", " + event.pageY;
 // });
+
+// #1 When the mouse moves within the body, console.log the x and y coordinates.
 $('body').mousemove(function(event) {
   var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
-
  console.log(pageCoords)
 })
+
+// #2 Instead of using console.log, replace the text of the h1 with the current coordinates of the mouse.
+
+$("#coords").html("pageCoords");
