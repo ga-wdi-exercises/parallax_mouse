a95a0f343517b611bbec61c8b70d5597f6b65b0e
Malcom Johnson <malcomjohnson@malcoms-MacBook-Pro.local>
2017-02-03 16:32:28 -0500

    second commit

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,4 +1,17 @@
 $('body').mousemove(function(event) {
   var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
  console.log(pageCoords)
+
+$("#coords").html(pageCoords);
+
+
+$("img").css("margin-left","margin-top");
+
+
+
+$('body').mousemove(function(event) {
+ $(".cat").css("margin-left", event.pageX/ 70).css("margin-top", event.pageY/ 30);
+ $(".dog").css("margin-left", event.pageX/ 10).css("margin-top", event.pageY/ 64);
+ $(".panda").css("margin-left", event.pageX/ 20).css("margin-top", event.pageY/ 95);
+});
 })
