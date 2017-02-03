0464173cc8aad4b73b436bb349da52bd7706f834
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-02-03 17:06:35 -0500

    bonus

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,19 +1,3 @@
-// // commmit 1)
-// $('body').mousemove(function(event) {
-//  var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
-// })
-// // replace H1 with pagecoords (commit 2)
-// $("#coords").html(pageCoords);
-// })
-//  commit 3
-// $('body').mousemove(function(event) {
-//  $(".cat").css("margin-left", event.pageX/ 2).css("margin-top", event.pageY/ 15);
-// })
-//   $(".dog").css("margin-left", event.pageX/2).css("margin-top", event.pageY/20);
-// })
-//     $(".panda").css("margin-left", event.pageX/2).css("margin-top", event.pageY/10);
-//   })
-
 // commit 1
 $('body').mousemove(function(event) {
  var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
@@ -33,4 +17,10 @@ $('body').mousemove(function(event) {
 // commit 4
 $("img").click(function() {
   $( this ).toggleClass("border")
+})
+
+// Bonus
+$("body").on("click", function(){
+  $(this).off()
+
 })
