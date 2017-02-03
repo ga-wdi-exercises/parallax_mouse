53dc6d3d5d2716b3d35200b0310ad2a09f741bd3
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-02-03 15:20:43 -0500

    commit 2

--- catdogpanda.js
+++ catdogpanda.js
@@ -4,9 +4,9 @@
 //   $("sapn:first").text("( e.pageX, e.pageY ) - " + pageCoords);
 //   $("span:last").text("(e.clientX, e.clientY ) - " + clientCoords)
 // });
-
+// commmit 1)
 $('body').mousemove(function(event) {
  var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
-
- console.log(pageCoords)
 })
+// replace H1 with pagecoords (commit 2)
+$("#cords").html("pageCoords")
