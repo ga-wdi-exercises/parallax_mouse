ec046a89a2021f4c28e2389ed794de495c967c2e
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-02-03 10:57:08 -0500

    second commit

--- catdogpanda.js
+++ catdogpanda.js
@@ -4,10 +4,21 @@
 //  console.log(coords);
 // })
 
+//Commit 1
+// $("body").mousemove(function(event){
+//   var msg = "Handler for .mousemove() called at ";
+//   msg += event.pageX + ", " + event.pageY;
+//   $( "#log" ).append( "<div>" + msg + "</div>" );
+//      console.log(msg)
+//  })
 
-$("body").mousemove(function(event){
-  var msg = "Handler for .mousemove() called at ";
-  msg += event.pageX + ", " + event.pageY;
-  $( "#log" ).append( "<div>" + msg + "</div>" );
-     console.log(msg)
- })
+ //Commit 2
+
+ $("#coords").mousemove(function(event){
+   var msg = "Handler for .mousemove() called at ";
+   msg += event.pageX + ", " + event.pageY;
+   $( "#log" ).append( "<div>" + msg + "</div>" );
+       console.log(msg)
+  })
+
+//commit 3
