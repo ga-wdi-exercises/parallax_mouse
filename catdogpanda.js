bef00ac1f77916b155e3a02ae6e27d31d05badcb
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-03 16:12:31 -0500

    set the css to change when the mouse moves for the margin-left and margin-top

--- catdogpanda.js
+++ catdogpanda.js
@@ -10,8 +10,22 @@ $('body').mousemove(function(event) {
  console.log(pageCoords)
 
 // #2 Instead of using console.log, replace the text of the h1 with the current coordinates of the mouse.
-
 $("#coords").html(pageCoords);
 
 // #3 When the mouse moves, change the css of each of your images so that the margin-left and margin-top change along with the x and y coordinates of your mouse.
+// $(".head-image").css("src","left.jpg");
+
+$('body').mousemove(function(event) {
+  $(".cat").css("margin-left", event.pageX/ 70).css("margin-top", event.pageY/ 30);
+  $(".dog").css("margin-left", event.pageX/ 10).css("margin-top", event.pageY/ 64);
+  $(".panda").css("margin-left", event.pageX/ 20).css("margin-top", event.pageY/ 95);
+});
+
+// #4 Have it so that when you click on any image, it either adds or removes the ".border" class
+
+// $("img").mousemove(function(event)) {
+//   var cssChange = "("margin-top", "margin-left", "event.pageX", "event.pageY")"
+//   console.log(cssChange)
+// }
+
 })
