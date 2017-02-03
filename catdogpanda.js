54c7db9f29fa9e9dc96d2fd3afca161754a3c193
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-02-03 12:25:08 -0500

    added commit 3

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,24 +1,21 @@
 
-// var newBody = $(document.body)
-// newbody.mouseover(function(){
-//  console.log(coords);
-// })
 
-//Commit 1
-// $("body").mousemove(function(event){
-//   var msg = "Handler for .mousemove() called at ";
-//   msg += event.pageX + ", " + event.pageY;
-//   $( "#log" ).append( "<div>" + msg + "</div>" );
-//      console.log(msg)
-//  })
+//commit 1
+$("body").mousemove(function(event){
+  var msg = "Handler for .mousemove() called at ";
+  msg += event.pageX + ", " + event.pageY;
+  $( "#log" ).append( "<div>" + msg + "</div>" );
+     console.log(msg)
+ })
 
  //Commit 2
 
- $("#coords").mousemove(function(event){
-   var msg = "Handler for .mousemove() called at ";
-   msg += event.pageX + ", " + event.pageY;
-   $( "#log" ).append( "<div>" + msg + "</div>" );
-       console.log(msg)
-  })
+ $("body").mousemove(function(event){
+   $("#coords").text("X"  + event.pageX + ",Y" + event.pageY);
+
+ });
 
 //commit 3
+$(".cat").css({"margin-left": event.pageX/6, "margin-top": event.pageY/6});
+$(".dog").css({"margin-left": event.page/12, "margin-top": event.pageY/12});
+$(".dog").css({"margin-left": event.page/12, "margin-top": event.pageY/12});
