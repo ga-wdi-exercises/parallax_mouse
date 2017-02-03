193928bd037791d35a4d05cacc30642b12767872
Dan Lin <dlin2102@gmail.com>
2017-02-03 16:06:49 -0500

    commit3

--- catdogpanda.js
+++ catdogpanda.js
@@ -2,4 +2,43 @@ $('body').mousemove(function(event) {
   var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
 
   $('h1').text(pageCoords);
+
+
+$('body').mousemove(function(event) {
+  $(".cat").css("margin-left", event.pageX/ 4).css("margin-top", event.pageY/ 9);
+});
+
+$('body').mousemove(function(event) {
+  $(".dog").css("margin-left", event.pageX/ 7).css("margin-top", event.pageY/ 18);
+});
+
+$('body').mousemove(function(event) {
+  $(".panda").css("margin-left", event.pageX/ 10).css("margin-top", event.pageY/ 12);
+});
+
+
+});
+
+/*$('.cat').mouseover(function() {
+    $(this).css('margin-left', 'marginLeftCat')
 })
+$('.cat').mouseover(function() {
+    $(this).css('margin-top', 'marginTopCat')
+})
+
+});
+
+  /*
+  $('.cat').mouseover(function() {
+      $(this).css($('margin-left', 'margin-top'), (pageCoords))
+  })
+
+  $('.panda').mouseover(function() {
+      $(this).css($('margin-left', 'margin-top'), (pageCoords))
+  })
+  $('.dog').mouseover(function() {
+      $(this).css($('margin-left', 'margin-top'), (pageCoords))
+  })
+
+})
+*/
