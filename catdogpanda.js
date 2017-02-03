36f47c951f6481ed071dbaaa3b201d04678ecdd0
Taylor Scott <TaylorScott@Taylors-MacBook-Pro.local>
2017-02-03 15:05:46 -0500

    number 3 done

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,6 +1,42 @@
-$( "body" ).mousemove(function( event ) {
-  var msg = " ";
+$("body").mousemove(function(event) {
+  var msg = "";
   msg += event.pageX + ", " + event.pageY;
   console.log(msg)
   $("h1").text( "Coordinates: " + msg);
 });
+
+$("body").on("mousemove", function() {
+  $(".cat").css({
+    "margin-top": function(mousemove) {
+      return parseFloat(event.pageY) * 0.1;
+    },
+    "margin-left": function(mousemove) {
+      return parseFloat(event.pageX) * 0.1;
+    }
+  });
+});
+
+$("body").on("mousemove", function() {
+  $(".dog").css({
+    "margin-top": function(mousemove) {
+      return parseFloat(event.pageY) * 0.1;
+    },
+    "margin-left": function(mousemove) {
+      return parseFloat(event.pageX) * 0.1;
+    }
+  });
+});
+
+$("body").on("mousemove", function() {
+  $(".panda").css({
+    "margin-top": function(mousemove) {
+      return parseFloat(event.pageY) * 0.05;
+    },
+    "margin-left": function(mousemove) {
+      return parseFloat(event.pageX) * 0.05;
+    }
+  });
+});
+
+
+//
