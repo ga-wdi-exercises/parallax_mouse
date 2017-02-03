f7804866132af2ac085b2fd4625f5a3f7f97c9fb
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-02-03 16:45:51 -0500

    commit 4

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,14 +1,36 @@
-// commmit 1)
+// // commmit 1)
+// $('body').mousemove(function(event) {
+//  var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
+// })
+// // replace H1 with pagecoords (commit 2)
+// $("#coords").html(pageCoords);
+// })
+//  commit 3
+// $('body').mousemove(function(event) {
+//  $(".cat").css("margin-left", event.pageX/ 2).css("margin-top", event.pageY/ 15);
+// })
+//   $(".dog").css("margin-left", event.pageX/2).css("margin-top", event.pageY/20);
+// })
+//     $(".panda").css("margin-left", event.pageX/2).css("margin-top", event.pageY/10);
+//   })
+
+// commit 1
 $('body').mousemove(function(event) {
  var pageCoords = "(" + event.pageX + ", " + event.pageY + ")";
-})
-// replace H1 with pagecoords (commit 2)
-$("#cords").html("pageCoords")
-//  commit 3
+console.log(pageCoords)
+
+// commit 2
+$("#coords").html(pageCoords);
+
+// commit 3
 $('body').mousemove(function(event) {
- $(".cat").css("margin-left", event.pageX/ 2).css("margin-top", event.pageY/ 15);
+ $(".cat").css("margin-left", event.pageX/ 2).css("margin-top", event.pageY/ 6);
+ $(".dog").css("margin-left", event.pageX/ 2).css("margin-top", event.pageY/ 20);
+ $(".panda").css("margin-left", event.pageX/ 2).css("margin-top", event.pageY/ 10);
 });
-  $(".dog").css("margin-left", event.pageX/2).css("margin-top", event.pageY/20);
-});
-    $(".panda").css("margin-left", event.pageX/2).css("margin-top", event.pageY/10);
-  });
+
+})
+// commit 4
+$("img").click(function() {
+  $( this ).toggleClass("border")
+})
